c489907795a0c85c8f2797d97f138f0fe1635bd3
cailinkless <cailin.nysff@gmail.com>
2021-01-05 13:23:32 -0500

    add list of cards at bottom

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ window.addEventListener("DOMContentLoaded", () => {
 let deck
 
 function getCards() {
+    let cardIndex = document.getElementById("card-index")
     fetch(BASE_URL + '/cards')
     .then(res => res.json())
     .then(cards => {
         deck = cards
+        deck.map(card => {
+            cardIndex.innerHTML += `
+            <li><a href="#" data-id="${card.id}">${card.number}. ${card.name}</a></li>
+            `
+        })
     })
 }
